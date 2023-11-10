3e5516c48f57366a07c029014db52d1e3231333f
amdadul-haque <amdadul1807107@gmail.com>
2023-11-10 11:51:10 +0600

    initial

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main>
+      <h2 className='text-center bg-black text-fuchsia-500 text-7xl'>here We go</h2>
+    </main>
+  )
+}
