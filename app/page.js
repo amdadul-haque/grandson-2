777dbc5d7435f38f217f00e55a2fca4d3ad5fdf3
Asfak Ahmed <asfakahmed680@gmail.com>
2023-11-12 15:34:58 +0600

    custom cursor added

--- app/page.js
+++ app/page.js
@@ -10,6 +10,7 @@ import SplashScreen from "@/components/SplashScreen";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
+import AnimatedCursor from "react-animated-cursor";
 
 export default function Home() {
   const pathname = usePathname();
@@ -40,6 +41,42 @@ export default function Home() {
           <Portfolio />
           <ContactUs />
           <Footer />
+          <AnimatedCursor
+            innerSize={8}
+            outerSize={17}
+            color="0, 189, 233"
+            outerAlpha={0.2}
+            innerStyle={{
+              backgroundColor: "rgb(0, 189, 233)",
+
+              borderRadius: "50%",
+              width: "50%",
+            }}
+            outerStyle={{
+              backgroundColor: "transparent",
+              border: "2px solid rgb(0, 189, 233)",
+              padding: "20px",
+              borderRadius: "50%",
+              width: "100px",
+            }}
+            showSystemCursor={true}
+            innerScale={0.7}
+            outerScale={5}
+            clickables={[
+              "a",
+              'input[type="text"]',
+              'input[type="email"]',
+              'input[type="number"]',
+              'input[type="submit"]',
+              'input[type="image"]',
+              "label[for]",
+              "select",
+              "textarea",
+              "li",
+              "button",
+              ".link",
+            ]}
+          />
         </main>
       )}
     </>
