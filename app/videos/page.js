6bfbb5efadf9cb641ec29fc16f34c72e53a7ef29
amdadul-haque <amdadul1807107@gmail.com>
2024-05-12 09:13:49 +0600

    text animation updated

--- app/videos/page.js
+++ app/videos/page.js
@@ -3,15 +3,14 @@ import React from 'react'
 
 const VideosPage = () => {
   return (
-    <>
+    <div className='overflow-hidden'>
       <Hero />
       <Section1 />
       <Section2 />
       <Section3 />
       <Section4 />
       {/* <Section5 /> */}
-
-    </>
+    </div>
   )
 }
 
