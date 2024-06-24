c061530c0de6bd5234afb7b19a75925f8ec15405
amdadul-haque <amdadul1807107@gmail.com>
2024-06-25 02:26:21 +0600

    updated

--- components/europa/StoryLines.js
+++ components/europa/StoryLines.js
@@ -1,4 +1,4 @@
-import { storylines } from '@/public/data/europa'
+import { hero, storylines } from '@/public/data/europa'
 import Image from 'next/image'
 import React from 'react'
 
@@ -10,7 +10,7 @@ const StoryLines = () => {
         <h2 className='text-purple-600 text-lg font-bold'>Politics Story Lines</h2>
       </div>
       <div className='w-full md:w-1/3 lg:w-full grid grid-cols-2 md:grid-cols-1 lg:grid-cols-5 gap-x-2 gap-y-4 '>
-        {
+        {/* {
           storylines.map((item, index) => (
             <div key={index} className='first-of-type:col-span-2 md:first-of-type:col-span-1'>
               <div className='rounded-lg overflow-hidden'>
@@ -21,7 +21,33 @@ const StoryLines = () => {
               </h1>
             </div>
           ))
-        }
+        } */}
+        {
+            hero.map((item, index) => (
+              <div key={index} className='first-of-type:col-span-2 md:first-of-type:col-span-1'>
+                <div className='rounded-lg overflow-hidden'>
+                  <Image src={item.image} alt={item.title} height={125} width={224} className='w-full aspect-video object-cover  hover:scale-105 transition' />
+                </div>
+                <h1 className='text-base mt-2 font-semibold'>
+                  {
+                    index == 0 && <><span className='text-red-euro'>KLĘSKA PUTINA - </span>dlaczego ruski watażka musi przegrać</>
+                  }
+                  {
+                    index == 1 && <> Top 10 <span className='text-red-euro'>PISowskich PRZEKRĘTÓW </span> jak polska skrajna prawica okradała ludzi</>
+                  }
+                  {
+                    index == 2 && <>LE PEN jest <span className='text-red-euro'> ZAGROŻENIEM</span></>
+                  }
+                  {
+                    index == 3 && <> <span className='text-blue-euro'> DONALD TUSK </span> Lider na trudne czasy</>
+                  }
+                  {
+                    index == 4 && <>Co daje nam Unia Europejska? <span className='text-blue-euro'>  - SONDA</span></>
+                  }
+                </h1>
+              </div>
+            ))
+          }
       </div>
     </div>
   )
