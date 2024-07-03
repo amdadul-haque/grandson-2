7d08212cf3692d65f8f8b599d7ad2ff5f75ea368
Grandson Media <123726752+GrandsonDasein@users.noreply.github.com>
2024-07-03 10:43:11 +0200

    Update Section2.js

--- components/media/Section2.js
+++ components/media/Section2.js
@@ -24,7 +24,7 @@ const Section2 = () => {
             <Image src='/images/media/dariusz_lukawski2.jpg' alt='Dariusz Łukawski' width={300} height={300} className="mx-auto mb-6" />
             <h3 className='section-title'> Dariusz Łukawski </h3>
             <p>
-              Trener wystąpień publicznych i medialnych, komunikacji kryzysowej, wykładowca  Uniwersytetu Warszawskiego, dziennikarz Teleexpressu. Od ponad 30 lat w mediach , głównie w TVP gdzie był reporterem, wydawcą, prezenterem i Redaktorem Naczelnym PANORAMY w telewizyjnej „Dwójce” oraz  redakcji publicystyki. 
+              Trener wystąpień publicznych <br /> i medialnych, komunikacji kryzysowej, wykładowca  Uniwersytetu Warszawskiego, dziennikarz Teleexpressu. Od ponad 30 lat w mediach , głównie w TVP gdzie był reporterem, wydawcą, prezenterem i Redaktorem Naczelnym PANORAMY w telewizyjnej „Dwójce” oraz  redakcji publicystyki. 
 Autor projektów szkoleń medialnych i komunikacji kryzysowej, z którego doświadczenia skorzystały setki menedżerów takich organizacji jak: Ministerstwo Finansów, Deutsche Bank, Ikea, Arval , AMGEN, EUROVIA, BNP Paribas, Konfederacja Pracodawców Lewiatan. 
 Trener mówców TEDx University of Warsaw.
             </p>
